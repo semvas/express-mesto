c743ae3376e901750e1ae08ebdc311db241df84e
IvanSergeyev <ivan.sergeyev29@yandex.ru>
2021-07-14 15:28:51 +0600

    fix routers errors

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ const {
 router.get('/users', getUsers);
 router.get('/users/:id', getUser);
 router.post('/users', createUser);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
+router.patch('/users/me', updateUser);
+router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
